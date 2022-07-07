46ec436fc2df3186ec6123b87cfb18e460b3ed42
Treasure Kabareebe <treasurecalm@gmail.com>
2022-07-08 01:08:05 +0300

    Update NewGame component with the mark outlines

--- src/components/NewGame.js
+++ src/components/NewGame.js
@@ -1,5 +1,7 @@
 import React from "react";
 import logo from "../assets/logo.svg";
+import outlineO from "../assets/icon-o-outline.svg";
+import outlineX from "../assets/icon-x-outline.svg";
 
 const NewGame = () => {
   return (
@@ -8,10 +10,18 @@ const NewGame = () => {
       <div className="mark-choice">
         <h1>PICK PLAYER 1'S MARK</h1>
         <form>
-          <input type="radio" id="x" name="mark" value="x" />
-          <label htmlFor="x">X</label>
-          <input type="radio" id="o" name="mark" value="o" />
-          <label htmlFor="o">O</label>
+          <div>
+            <input type="radio" id="x" name="mark" value="x" />
+            <label htmlFor="x">
+              <img src={outlineX} alt="x" className="outline" />
+            </label>
+          </div>
+          <div>
+            <input type="radio" id="o" name="mark" value="o" />
+            <label htmlFor="o">
+              <img src={outlineO} alt="o" className="outline" />
+            </label>
+          </div>
         </form>
         <p>REMEMBER : X GOES FIRST</p>
       </div>
